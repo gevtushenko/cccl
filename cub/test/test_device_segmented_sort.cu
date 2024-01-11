b256a36495e7880253b0e65e696c084946282b69
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2024-01-11 01:20:19 -0800

    A bit simpler

--- cub/test/test_device_segmented_sort.cu
+++ cub/test/test_device_segmented_sort.cu
@@ -43,59 +43,7 @@
 #include <thrust/shuffle.h>
 #include <thrust/sort.h>
 
-#include <fstream>
-
-#define TEST_HALF_T !_NVHPC_CUDA
-
-#define TEST_BF_T !_NVHPC_CUDA
-
-#if TEST_HALF_T
-#include <cuda_fp16.h>
-#endif
-
-#if TEST_BF_T
-#include <cuda_bf16.h>
-#endif
-
 using namespace cub;
-
-template <typename T>
-struct UnwrapHalfAndBfloat16
-{
-  using Type = T;
-};
-
-#if TEST_HALF_T
-template <>
-struct UnwrapHalfAndBfloat16<half_t>
-{
-  using Type = __half;
-};
-#endif
-
-#if TEST_BF_T
-template <>
-struct UnwrapHalfAndBfloat16<bfloat16_t>
-{
-  using Type = __nv_bfloat16;
-};
-#endif
-
-static constexpr int MAX_ITERATIONS = 2;
-
-
-class SizeGroupDescription
-{
-public:
-  SizeGroupDescription(const int segments,
-                       const int segment_size)
-      : segments(segments)
-      , segment_size(segment_size)
-  {}
-
-  int segments {};
-  int segment_size {};
-};
 
 template <typename KeyT>
 struct SegmentChecker
@@ -410,242 +358,6 @@ private:
   }
 };
 
-template <typename KeyT,
-          bool IsIntegralType = std::is_integral<KeyT>::value>
-class InputDescription
-{
-  thrust::host_vector<int> segment_sizes;
-
-public:
-  InputDescription& add(const SizeGroupDescription &group)
-  {
-    if (static_cast<std::size_t>(group.segment_size) <
-        static_cast<std::size_t>((std::numeric_limits<KeyT>::max)()))
-    {
-      for (int i = 0; i < group.segments; i++)
-      {
-        segment_sizes.push_back(group.segment_size);
-      }
-    }
-
-    return *this;
-  }
-
-  template <typename ValueT = cub::NullType>
-  Input<KeyT, ValueT> gen(bool reverse)
-  {
-    return Input<KeyT, ValueT>(reverse, segment_sizes);
-  }
-};
-
-template <typename KeyT>
-class InputDescription<KeyT, false>
-{
-  thrust::host_vector<int> segment_sizes;
-
-public:
-  InputDescription& add(const SizeGroupDescription &group)
-  {
-    for (int i = 0; i < group.segments; i++)
-    {
-      segment_sizes.push_back(group.segment_size);
-    }
-
-    return *this;
-  }
-
-  template <typename ValueT = cub::NullType>
-  Input<KeyT, ValueT> gen(bool reverse)
-  {
-    return Input<KeyT, ValueT>(reverse, segment_sizes);
-  }
-};
-
-constexpr bool keys_only = false;
-constexpr bool pairs = true;
-
-constexpr bool ascending = false;
-constexpr bool descending = true;
-
-constexpr bool pointers = false;
-constexpr bool double_buffer = true;
-
-constexpr bool unstable = false;
-constexpr bool stable = true;
-
-template <typename KeyT,
-          typename ValueT>
-void TestSameSizeSegments(int segment_size,
-                          int segments,
-                          bool skip_values = false)
-{
-  const int num_items = segment_size * segments;
-
-  thrust::device_vector<int> offsets(segments + 1);
-  thrust::sequence(offsets.begin(),
-                   offsets.end(),
-                   int{},
-                   segment_size);
-
-  const int *d_offsets = thrust::raw_pointer_cast(offsets.data());
-
-  const KeyT target_key {1};
-  const ValueT target_value {42};
-
-  thrust::device_vector<KeyT> keys_input(num_items);
-  thrust::device_vector<KeyT> keys_output(num_items);
-
-  KeyT *d_keys_input  = thrust::raw_pointer_cast(keys_input.data());
-  KeyT *d_keys_output = thrust::raw_pointer_cast(keys_output.data());
-
-  thrust::device_vector<ValueT> values_input(num_items);
-  thrust::device_vector<ValueT> values_output(num_items);
-
-  thrust::host_vector<KeyT> host_keys(num_items);
-  thrust::host_vector<ValueT> host_values(num_items);
-
-  ValueT *d_values_input  = thrust::raw_pointer_cast(values_input.data());
-  ValueT *d_values_output = thrust::raw_pointer_cast(values_output.data());
-
-  for (bool stable_sort: { unstable, stable })
-  {
-    for (bool sort_pairs: { keys_only, pairs })
-    {
-      if (sort_pairs)
-      {
-        if (skip_values)
-        {
-          continue;
-        }
-      }
-
-      for (bool sort_descending: { ascending, descending })
-      {
-        for (bool sort_buffers: { pointers, double_buffer })
-        {
-          cub::DoubleBuffer<KeyT> keys_buffer(nullptr, nullptr);
-          cub::DoubleBuffer<ValueT> values_buffer(nullptr, nullptr);
-          values_buffer.selector = 1;
-
-          thrust::fill(keys_input.begin(), keys_input.end(), target_key);
-          thrust::fill(keys_output.begin(), keys_output.end(), KeyT{});
-
-          if (sort_pairs)
-          {
-            if (sort_buffers)
-            {
-              thrust::fill(values_input.begin(), values_input.end(), ValueT{});
-              thrust::fill(values_output.begin(), values_output.end(), target_value);
-            }
-            else
-            {
-              thrust::fill(values_input.begin(), values_input.end(), target_value);
-              thrust::fill(values_output.begin(), values_output.end(), ValueT{});
-            }
-          }
-
-          // If temporary storage size is defined by extra keys storage
-          {
-            host_keys = keys_buffer.selector || !sort_buffers ? keys_output
-                                                              : keys_input;
-            const std::size_t items_selected =
-              thrust::count(host_keys.begin(), host_keys.end(), target_key);
-          }
-
-          if (sort_pairs)
-          {
-            host_values = values_buffer.selector || !sort_buffers
-                            ? values_output
-                            : values_input;
-            const std::size_t items_selected =
-              thrust::count(host_values.begin(),
-                            host_values.end(),
-                            target_value);
-          }
-        }
-      }
-    }
-  }
-}
-
-
-template <typename KeyT,
-          typename ValueT>
-void InputTest(bool sort_descending,
-               Input<KeyT, ValueT> &input)
-{
-  thrust::device_vector<KeyT> keys_output(input.get_num_items());
-  KeyT *d_keys_output = thrust::raw_pointer_cast(keys_output.data());
-
-  thrust::device_vector<ValueT> values_output(input.get_num_items());
-  ValueT *d_values_output = thrust::raw_pointer_cast(values_output.data());
-
-  for (bool stable_sort: { unstable, stable })
-  {
-    for (bool sort_pairs : { keys_only, pairs })
-    {
-      for (bool sort_buffers : {pointers, double_buffer})
-      {
-        for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++)
-        {
-          thrust::fill(keys_output.begin(), keys_output.end(), KeyT{});
-          thrust::fill(values_output.begin(), values_output.end(), ValueT{});
-
-          input.shuffle();
-        }
-      }
-    }
-  }
-}
-
-struct ComparisonPredicate
-{
-  template <typename T>
-  __host__ __device__ bool operator()(const T &lhs, const T &rhs) const
-  {
-    return lhs == rhs;
-  }
-
-  __host__ __device__ bool operator()(const half_t &lhs, const half_t &rhs) const
-  {
-    return lhs.raw() == rhs.raw();
-  }
-};
-
-template <typename T>
-bool compare_two_outputs(const thrust::host_vector<int> &offsets,
-                         const thrust::host_vector<T> &lhs,
-                         const thrust::host_vector<T> &rhs)
-{
-  const auto num_segments = static_cast<unsigned int>(offsets.size() - 1);
-
-  for (std::size_t segment_id = 0; segment_id < num_segments; segment_id++)
-  {
-    auto lhs_begin = lhs.cbegin() + offsets[segment_id];
-    auto lhs_end = lhs.cbegin() + offsets[segment_id + 1];
-    auto rhs_begin = rhs.cbegin() + offsets[segment_id];
-
-    auto err = thrust::mismatch(lhs_begin, lhs_end, rhs_begin, ComparisonPredicate{});
-
-    if (err.first != lhs_end)
-    {
-      const auto idx = thrust::distance(lhs_begin, err.first);
-      const auto segment_size = std::distance(lhs_begin, lhs_end);
-
-      std::cerr << "Mismatch in segment " << segment_id
-                << " at position " << idx << " / " << segment_size
-                << ": "
-                << static_cast<std::uint64_t>(lhs_begin[idx]) << " vs "
-                << static_cast<std::uint64_t>(rhs_begin[idx]) << " ("
-                << typeid(lhs_begin[idx]).name() << ")" << std::endl;
-
-      return false;
-    }
-  }
-
-  return true;
-}
-
 template <typename ValueT>
 void RandomizeInput(thrust::host_vector<bool> &h_keys,
                     thrust::host_vector<ValueT> &h_values)
@@ -668,31 +380,6 @@ void RandomizeInput(thrust::host_vector<KeyT> &h_keys,
     h_values[i] = RandomValue((std::numeric_limits<ValueT>::max)());
   }
 }
-
-#if TEST_HALF_T
-void RandomizeInput(thrust::host_vector<half_t> &h_keys,
-                    thrust::host_vector<std::uint32_t> &h_values)
-{
-  for (std::size_t i = 0; i < h_keys.size(); i++)
-  {
-    h_keys[i] = RandomValue((std::numeric_limits<int>::max)());
-    h_values[i] = RandomValue((std::numeric_limits<std::uint32_t>::max)());
-  }
-}
-#endif
-
-#if TEST_BF_T
-void RandomizeInput(thrust::host_vector<bfloat16_t> &h_keys,
-                    thrust::host_vector<std::uint32_t> &h_values)
-{
-  for (std::size_t i = 0; i < h_keys.size(); i++)
-  {
-    h_keys[i] = RandomValue((std::numeric_limits<int>::max)());
-    h_values[i] = RandomValue((std::numeric_limits<std::uint32_t>::max)());
-  }
-}
-#endif
-
 
 
 template <typename KeyT,
@@ -763,7 +450,7 @@ void InputTestRandom(Input<KeyT, ValueT> &input)
 
   const thrust::host_vector<int> &h_offsets = input.get_h_offsets();
 
-  for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++)
+  for (int iteration = 0; iteration < 2; iteration++)
   {
     RandomizeInput(h_keys, h_values);
 
@@ -815,12 +502,12 @@ void RandomTest(int min_segments,
 {
   constexpr int max_items = 10000000;
 
-  for (int iteration = 0; iteration < MAX_ITERATIONS; iteration++)
+  for (int iteration = 0; iteration < 2; iteration++)
   {
     Input<KeyT, ValueT> edge_cases = GenRandomInput<KeyT, ValueT>(max_items,
                                                                   min_segments,
                                                                   max_segments,
-                                                                  descending);
+                                                                  false);
 
     InputTestRandom(edge_cases);
   }
