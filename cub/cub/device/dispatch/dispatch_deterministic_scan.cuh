9016446fa6bc9710a997b1aac358a246b67cef77
Georgy Evtushenko <evtushenko.georgy@gmail.com>
2024-01-27 00:53:08 +0000

    Outline

--- cub/cub/device/dispatch/dispatch_deterministic_scan.cuh
+++ cub/cub/device/dispatch/dispatch_deterministic_scan.cuh
@@ -37,7 +37,7 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cub/agent/agent_scan.cuh>
+#include <cub/agent/agent_reduce.cuh>
 #include <cub/device/dispatch/tuning/tuning_scan.cuh>
 #include <cub/grid/grid_queue.cuh>
 #include <cub/thread/thread_operators.cuh>
@@ -56,28 +56,112 @@ namespace detail
 namespace run_to_run_deterministic_scan
 {
 
+template <class AccumT>
+struct policy_hub_t 
+{
+  /// SM35
+  struct policy_350_t : ChainedPolicy<350, policy_350_t, policy_350_t>
+  {
+    static constexpr int threads_per_block  = 256;
+    static constexpr int items_per_thread   = 20;
+    static constexpr int items_per_vec_load = 4;
+
+    using ReducePolicy = AgentReducePolicy<threads_per_block,
+                                           items_per_thread,
+                                           AccumT,
+                                           items_per_vec_load,
+                                           BLOCK_REDUCE_WARP_REDUCTIONS,
+                                           LOAD_DEFAULT>;
+  };
+
+  using MaxPolicy = policy_350_t;
+};
+
 template <class ChainedPolicyT,
           class InputIteratorT,
           class ScanOpT,
-          class InitValueT,
           class OffsetT,
           class AccumT>
-__launch_bounds__(int(ChainedPolicyT::ActivePolicy::ScanPolicyT::BLOCK_THREADS))
+__launch_bounds__(int(ChainedPolicyT::ActivePolicy::ReducePolicy::BLOCK_THREADS))
   CUB_DETAIL_KERNEL_ATTRIBUTES void reduce_kernel(
     InputIteratorT d_in,
     AccumT *d_tile_aggregates,
     ScanOpT scan_op,
-    InitValueT init_value,
     OffsetT num_items)
 {
   // thread block reduces tile to one aggregate
+  using policy_t = typename ChainedPolicyT::ActivePolicy::ReducePolicy;
+
+  constexpr auto tile_items = policy_t::BLOCK_THREADS * policy_t::ITEMS_PER_THREAD;
+
+  const auto tile_id    = static_cast<OffsetT>(blockIdx.x);
+  const auto tile_begin = tile_id * tile_items;
+  const auto tile_end   = tile_begin + tile_items ? num_items : tile_begin + tile_items;
+
+  using AgentReduceT =
+    AgentReduce<typename ChainedPolicyT::ActivePolicy::ReducePolicy,
+                InputIteratorT,
+                AccumT*,
+                OffsetT,
+                ScanOpT,
+                AccumT,
+                ::cuda::std::__identity>;
+
+  __shared__ typename AgentReduceT::TempStorage temp_storage;
+
+  AccumT block_aggregate =
+    AgentReduceT(temp_storage, d_in, scan_op, ::cuda::std::__identity{}).ConsumeTiles(tile_begin, tile_end);
+
+  if (threadIdx.x == 0)
+  {
+    detail::uninitialized_copy(d_tile_aggregates + tile_id, block_aggregate);
+  }
 }
 
-template <class ChainedPolicyT, class ScanOpT, class AccumT, class OffsetT>
+template <class ChainedPolicyT, class InitValueT, class ScanOpT, class AccumT, class OffsetT>
 __launch_bounds__(int(ChainedPolicyT::ActivePolicy::ScanPolicyT::BLOCK_THREADS))
-CUB_DETAIL_KERNEL_ATTRIBUTES void tile_scan_kernel(AccumT *d_tile_aggregates, ScanOpT scan_op, OffsetT num_tiles)
+  CUB_DETAIL_KERNEL_ATTRIBUTES void tile_scan_kernel(
+    AccumT* d_tile_aggregates, ScanOpT scan_op, InitValueT init_value, OffsetT num_tiles)
 {
+  using policy_t                  = typename ChainedPolicyT::ActivePolicy::ReducePolicy;
+  constexpr auto items_per_thread = policy_t::BLOCK_THREADS;
+  constexpr auto block_threads    = policy_t::ITEMS_PER_THREAD;
+  constexpr bool is_inclusive     = ::cuda::std::is_same<InitValueT, NullType>::value;
+
   // single thread block computes prefix sum of tile aggregates
+  using real_init_value_t = typename InitValueT::value_type;
+  using scan_t            = BlockScan<AccumT, block_threads>;
+  using temp_storage_t    = typename scan_t::TempStorage;
+
+  __shared__ temp_storage_t temp_storage;
+
+  constexpr auto tile_items = block_threads * items_per_thread;
+
+  real_init_value_t real_init_value = init_value;
+  BlockScanRunningPrefixOp<AccumT, ScanOpT> prefix_op(scan_op);
+  prefix_op.running_total = real_init_value;
+
+  OffsetT offset = 0;
+  while (offset < num_tiles) 
+  {
+    const auto valid_items = CUB_MIN(num_tiles - offset, tile_items);
+
+    // TODO block load
+    AccumT items[items_per_thread];
+    LoadDirectBlocked(threadIdx.x, d_tile_aggregates + offset, items, valid_items);
+
+    if (is_inclusive)
+    {
+      scan_t(temp_storage).InclusiveScan(items, items, scan_op, prefix_op);
+    }
+    else
+    {
+      scan_t(temp_storage).ExclusiveScan(items, items, scan_op, prefix_op);
+    }
+    CTA_SYNC();
+
+    StoreDirectBlocked(threadIdx.x, d_tile_aggregates + offset, items, valid_items);
+  }
 }
 
 // TODO Iterate tiles in reverse order to utilize some cache left from the first pass
@@ -90,20 +174,45 @@ template <class ChainedPolicyT,
 __launch_bounds__(int(ChainedPolicyT::ActivePolicy::ScanPolicyT::BLOCK_THREADS))
   CUB_DETAIL_KERNEL_ATTRIBUTES void scan_kernel(
     InputIteratorT d_in,
-    AccumT d_tile_aggregates,
+    AccumT *d_tile_aggregates,
     OutputIteratorT d_out,
     ScanOpT scan_op,
     OffsetT num_items)
 {
   // thread block scans tile and writes results to output
+  using policy_t                  = typename ChainedPolicyT::ActivePolicy::ReducePolicy;
+  constexpr auto items_per_thread = policy_t::BLOCK_THREADS;
+  constexpr auto block_threads    = policy_t::ITEMS_PER_THREAD;
+
+  using scan_t            = BlockScan<AccumT, block_threads>;
+  using temp_storage_t    = typename scan_t::TempStorage;
+
+  __shared__ temp_storage_t temp_storage;
+
+  constexpr auto tile_items = block_threads * items_per_thread;
+
+  BlockScanRunningPrefixOp<AccumT, ScanOpT> prefix_op(scan_op);
+  prefix_op.running_total = d_tile_aggregates[blockIdx.x];
+
+  OffsetT offset = 0;
+  const auto valid_items = CUB_MIN(num_items - offset, tile_items);
+
+  // TODO block load
+  AccumT items[items_per_thread];
+  LoadDirectBlocked(threadIdx.x, d_in + offset, items, valid_items);
+
+  scan_t(temp_storage).ExclusiveScan(items, items, scan_op, prefix_op);
+  CTA_SYNC();
+
+  StoreDirectBlocked(threadIdx.x, d_out + offset, items, valid_items);
 }
 
-template <typename InputIteratorT,
-          typename OutputIteratorT,
-          typename ScanOpT,
-          typename InitValueT,
-          typename OffsetT,
-          typename AccumT = //
+template <class InputIteratorT,
+          class OutputIteratorT,
+          class ScanOpT,
+          class InitValueT,
+          class OffsetT,
+          class AccumT = //
           detail::accumulator_t<ScanOpT,
                                 cub::detail::conditional_t<std::is_same<InitValueT, NullType>::value,
                                                            cub::detail::value_t<InputIteratorT>,
@@ -146,32 +255,25 @@ struct dispatch_t : SelectedPolicy
       , ptx_version(ptx_version)
   {}
 
-  template <typename ActivePolicyT, typename ReduceKernel, typename ScanKernel>
+  template <class ActivePolicyT, class ReduceKernel, class TileScanKernel, class ScanKernel>
   CUB_RUNTIME_FUNCTION _CCCL_HOST _CCCL_FORCEINLINE cudaError_t
-  Invoke(ReduceKernel reduce_kernel, ScanKernel scan_kernel)
+  Invoke(ReduceKernel reduce, TileScanKernel tile_scan, ScanKernel scan)
   {
-    typedef typename ActivePolicyT::ScanPolicyT Policy;
+    using policy_t = typename ActivePolicyT::ReducePolicy;
 
     // `LOAD_LDG` makes in-place execution UB and doesn't lead to better
     // performance.
-    static_assert(Policy::LOAD_MODIFIER != CacheLoadModifier::LOAD_LDG,
-                  "The memory consistency model does not apply to texture "
-                  "accesses");
+    // static_assert(policy_t::LOAD_MODIFIER != CacheLoadModifier::LOAD_LDG,
+    //               "The memory consistency model does not apply to texture "
+    //               "accesses");
 
     cudaError error = cudaSuccess;
     do
     {
-      // Get device ordinal
-      int device_ordinal;
-      error = CubDebug(cudaGetDevice(&device_ordinal));
-      if (cudaSuccess != error)
-      {
-        break;
-      }
-
       // Number of input tiles
-      int tile_size = Policy::BLOCK_THREADS * Policy::ITEMS_PER_THREAD;
-      int num_tiles = static_cast<int>(cub::DivideAndRoundUp(num_items, tile_size));
+      constexpr auto block_threads = policy_t::BLOCK_THREADS;
+      constexpr auto tile_size = block_threads * policy_t::ITEMS_PER_THREAD;
+      const int num_tiles      = static_cast<int>(cub::DivideAndRoundUp(num_items, tile_size));
 
       // Specify temporary storage allocation requirements
       size_t allocation_sizes[1];
@@ -187,7 +289,7 @@ struct dispatch_t : SelectedPolicy
         break;
       }
 
-      if (d_temp_storage == NULL)
+      if (d_temp_storage == nullptr)
       {
         // Return if the caller is simply requesting the size of the storage
         // allocation
@@ -200,13 +302,14 @@ struct dispatch_t : SelectedPolicy
         break;
       }
 
-#ifdef CUB_DETAIL_DEBUG_ENABLE_LOG
-      _CubLog("Invoking init_kernel<<<%d, %d, 0, %lld>>>()\n", init_grid_size, INIT_KERNEL_THREADS, (long long) stream);
-#endif
+      AccumT* d_tile_aggregates = reinterpret_cast<AccumT*>(allocations[0]);
 
-      // Invoke init_kernel to initialize tile descriptors
-      THRUST_NS_QUALIFIER::cuda_cub::launcher::triple_chevron(init_grid_size, INIT_KERNEL_THREADS, 0, stream)
-        .doit(init_kernel, tile_state, num_tiles);
+#ifdef CUB_DETAIL_DEBUG_ENABLE_LOG
+      _CubLog("Invoking reduce_kernel<<<%d, %d, 0, %lld>>>()\n", num_tiles, block_threads, (long long) stream);
+#endif
+      // Invoke reduce_kernel to get tile aggregates
+      THRUST_NS_QUALIFIER::cuda_cub::launcher::triple_chevron(num_tiles, block_threads, 0, stream)
+        .doit(reduce, d_in, d_tile_aggregates, scan_op, num_items);
 
       // Check for failure to launch
       error = CubDebug(cudaPeekAtLastError());
@@ -222,57 +325,46 @@ struct dispatch_t : SelectedPolicy
         break;
       }
 
-      // Get SM occupancy for scan_kernel
-      int scan_sm_occupancy;
-      error = CubDebug(MaxSmOccupancy(scan_sm_occupancy, // out
-                                      scan_kernel,
-                                      Policy::BLOCK_THREADS));
-      if (cudaSuccess != error)
-      {
-        break;
-      }
-
-      // Get max x-dimension of grid
-      int max_dim_x;
-      error = CubDebug(cudaDeviceGetAttribute(&max_dim_x, cudaDevAttrMaxGridDimX, device_ordinal));
-      if (cudaSuccess != error)
-      {
-        break;
-      }
-
-      // Run grids in epochs (in case number of tiles exceeds max x-dimension
-      int scan_grid_size = CUB_MIN(num_tiles, max_dim_x);
-      for (int start_tile = 0; start_tile < num_tiles; start_tile += scan_grid_size)
-      {
-// Log scan_kernel configuration
 #ifdef CUB_DETAIL_DEBUG_ENABLE_LOG
-        _CubLog("Invoking %d scan_kernel<<<%d, %d, 0, %lld>>>(), %d items "
-                "per thread, %d SM occupancy\n",
-                start_tile,
-                scan_grid_size,
-                Policy::BLOCK_THREADS,
-                (long long) stream,
-                Policy::ITEMS_PER_THREAD,
-                scan_sm_occupancy);
+      _CubLog("Invoking tile_scan_kernel<<<1, 1024, 0, %lld>>>()\n", (long long) stream);
 #endif
+      // Invoke tile_scan_kernel to turn tile aggregates into tile prefixes
+      THRUST_NS_QUALIFIER::cuda_cub::launcher::triple_chevron(1, 1024, 0, stream)
+        .doit(d_tile_aggregates, scan_op, init_value, num_tiles);
 
-        // Invoke scan_kernel
-        THRUST_NS_QUALIFIER::cuda_cub::launcher::triple_chevron(scan_grid_size, Policy::BLOCK_THREADS, 0, stream)
-          .doit(scan_kernel, d_in, d_out, tile_state, start_tile, scan_op, init_value, num_items);
+      // Check for failure to launch
+      error = CubDebug(cudaPeekAtLastError());
+      if (cudaSuccess != error)
+      {
+        break;
+      }
 
-        // Check for failure to launch
-        error = CubDebug(cudaPeekAtLastError());
-        if (cudaSuccess != error)
-        {
-          break;
-        }
+      // Sync the stream if specified to flush runtime errors
+      error = CubDebug(detail::DebugSyncStream(stream));
+      if (cudaSuccess != error)
+      {
+        break;
+      }
 
-        // Sync the stream if specified to flush runtime errors
-        error = CubDebug(detail::DebugSyncStream(stream));
-        if (cudaSuccess != error)
-        {
-          break;
-        }
+#ifdef CUB_DETAIL_DEBUG_ENABLE_LOG
+      _CubLog("Invoking scan_kernel<<<%d, %d, 0, %lld>>>()\n", num_tiles, block_threads, (long long) stream);
+#endif
+      // Invoke tile_scan_kernel to turn tile aggregates into tile prefixes
+      THRUST_NS_QUALIFIER::cuda_cub::launcher::triple_chevron(num_tiles, block_threads, 0, stream)
+        .doit(d_in, d_tile_aggregates, d_out, scan_op, num_items);
+
+      // Check for failure to launch
+      error = CubDebug(cudaPeekAtLastError());
+      if (cudaSuccess != error)
+      {
+        break;
+      }
+
+      // Sync the stream if specified to flush runtime errors
+      error = CubDebug(detail::DebugSyncStream(stream));
+      if (cudaSuccess != error)
+      {
+        break;
       }
     } while (0);
 
@@ -282,12 +374,10 @@ struct dispatch_t : SelectedPolicy
   template <typename ActivePolicyT>
   CUB_RUNTIME_FUNCTION _CCCL_HOST _CCCL_FORCEINLINE cudaError_t Invoke()
   {
-    typedef typename dispatch_t::MaxPolicy MaxPolicyT;
-    typedef typename cub::ScanTileState<AccumT> ScanTileStateT;
-    // Ensure kernels are instantiated.
-    return Invoke<ActivePolicyT>(
-      reduce_kernel<ScanTileStateT>,
-      scan_kernel<MaxPolicyT, InputIteratorT, OutputIteratorT, ScanTileStateT, ScanOpT, InitValueT, OffsetT, AccumT>);
+    using max_policy_t = typename dispatch_t::MaxPolicy;
+    return Invoke<ActivePolicyT>(reduce_kernel<max_policy_t, InputIteratorT, ScanOpT, OffsetT, AccumT>,
+                                 tile_scan_kernel<max_policy_t, InitValueT, ScanOpT, AccumT, OffsetT>,
+                                 scan_kernel<max_policy_t, InputIteratorT, OutputIteratorT, ScanOpT, OffsetT, AccumT>);
   }
 
   CUB_RUNTIME_FUNCTION _CCCL_FORCEINLINE static cudaError_t dispatch(
