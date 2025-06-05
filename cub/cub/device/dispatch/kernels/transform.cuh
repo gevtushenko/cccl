447ec2faabdbd0023ac77ad7f977d60ddb1af824
Michał 'Griwes' Dominiak <griwes@griwes.info>
2025-06-05 20:34:56 +0200

    post-review-wip

--- cub/cub/device/dispatch/kernels/transform.cuh
+++ cub/cub/device/dispatch/kernels/transform.cuh
@@ -260,6 +260,8 @@ _CCCL_DEVICE void transform_kernel_ublkcp(
         const int bytes_to_copy = round_up_to_po2_multiple(
           aligned_ptr.head_padding + static_cast<int>(sizeof(T)) * tile_stride, bulk_copy_size_multiple);
 
+        _CCCL_ASSERT(__isShared(dst), "");
+        _CCCL_ASSERT(__isGlobal(src), "");
         ::cuda::ptx::cp_async_bulk(::cuda::ptx::space_cluster, ::cuda::ptx::space_global, dst, src, bytes_to_copy, &bar);
         total_copied += bytes_to_copy;
 
